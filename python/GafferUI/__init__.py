4fb6399dc24fb16e1de800f9554baf5dab66b8d3
john <john@d3a5e123-01df-480b-a3e5-5969b2df5ebc>
2007-12-30 07:19:16 +0000

    Added handling of errors during gtk startup.

--- python/GafferUI/__init__.py
+++ python/GafferUI/__init__.py
@@ -1,5 +1,26 @@
-import pygtk
-pygtk.require( "2.0" )
+###################################################################################################
+# try to import gtk, handling any errors as gracefully as we can
+###################################################################################################
+import sys
+import IECore
+
+import warnings
+warnings.filterwarnings( "error", "could not open display", Warning, "gtk" )
+
+try :
+
+	import pygtk
+	pygtk.require( "2.0" )
+	import gtk
+
+except Exception, e :
+
+	IECore.msg( IECore.Msg.Level.Error, "GafferUI", "Unable to initialise UI toolkit (%s)." % e )
+	sys.exit( 1 )
+
+###################################################################################################
+# gtk imported ok. bring in the GafferUI components
+###################################################################################################
 
 from Menu import Menu
 from ScriptEditor import ScriptEditor
@@ -11,9 +32,9 @@ from PlugWidget import PlugWidget
 
 ## \todo Move the settings munging somewhere else
 # - perhaps this is best done by the Preferences node?
-import gtk
 settings = gtk.settings_get_default()
 
+# stop gtk menus feeling sluggish
 m = gtk.Menu() # have to make a menu for the property to be created
 del m
 settings.set_property( "gtk-menu-popup-delay", 0 )
