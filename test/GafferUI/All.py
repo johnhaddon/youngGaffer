ff6180bf0433a0cd26ee12b86e03fd735201cfce
john <john@d3a5e123-01df-480b-a3e5-5969b2df5ebc>
2008-03-16 05:45:08 +0000

    Added a test to the main suite.

--- test/GafferUI/All.py
+++ test/GafferUI/All.py
@@ -4,6 +4,7 @@ from MenuTest import *
 from PanelTest import *
 from WindowTest import *
 from ListContainerTest import *
+from EventSignalCombinerTest import *
 
 if __name__ == "__main__":
 	unittest.main()
