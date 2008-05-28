b8bb4b89267ac66111ca78b72c808c442f548181
john <john@d3a5e123-01df-480b-a3e5-5969b2df5ebc>
2008-05-28 02:35:09 +0000

    Now draws the whole frame.

--- src/GafferUI/Frame.cpp
+++ src/GafferUI/Frame.cpp
@@ -9,11 +9,13 @@
 using namespace GafferUI;
 using namespace IECore;
 using namespace Imath;
+using namespace boost;
 
 static const int cornerSegments = 10;
 
 /// \todo I suspect there should be a central resource class providing these shapes to all
-/// Gadgets.
+/// Gadgets. Maybe it's a Style class? And it has methods to draw these elements straight
+/// to a renderer?
 static CurvesPrimitivePtr cornerCurve()
 {
 	static CurvesPrimitivePtr c = 0;
@@ -60,6 +62,64 @@ static MeshPrimitivePtr cornerMesh()
 	return m;
 }
 
+/*static CurvesPrimitivePtr horizonalLine()
+{
+	static CurvesPrimitivePtr c = 0;
+	if( !c )
+	{
+s		IntVectorDataPtr vertsPerCurve = new IntVectorData;
+		vertsPerCurve->writable().push_back( 2 );
+		
+		V3fVectorDataPtr points = new V3fVectorData;
+		points->writable().push_back( V3f( 0, 0, 0  ) );
+		points->writable().push_back( V3f( 1, 0, 0  ) );
+		c = new CurvesPrimitive( vertsPerCurve, CubicBasisf::linear(), false, points );
+	}
+	return c;
+}
+
+static CurvesPrimitivePtr verticalLine()
+{
+	static CurvesPrimitivePtr c = 0;
+	if( !c )
+	{
+		IntVectorDataPtr vertsPerCurve = new IntVectorData;
+		vertsPerCurve->writable().push_back( 2 );
+		
+		V3fVectorDataPtr points = new V3fVectorData;
+		points->writable().push_back( V3f( 0, 0, 0  ) );
+		points->writable().push_back( V3f( 0, 1, 0  ) );
+		c = new CurvesPrimitive( vertsPerCurve, CubicBasisf::linear(), false, points );
+	}
+	return c;
+}*/
+
+static MeshPrimitivePtr rectangleMesh()
+{
+	static MeshPrimitivePtr m = 0;
+	if( !m )
+	{
+		IntVectorDataPtr vertsPerFace = new IntVectorData;
+		vertsPerFace->writable().push_back( 4 );
+		
+		V3fVectorDataPtr points = new V3fVectorData;
+		IntVectorDataPtr vertIds = new IntVectorData;
+		
+		points->writable().push_back( V3f( 0, 0, 0 ) );
+		points->writable().push_back( V3f( 1, 0, 0 ) );
+		points->writable().push_back( V3f( 1, 1, 0 ) );
+		points->writable().push_back( V3f( 0, 1, 0 ) );
+		
+		vertIds->writable().push_back( 0 );
+		vertIds->writable().push_back( 1 );
+		vertIds->writable().push_back( 2 );
+		vertIds->writable().push_back( 3 );
+		
+		m = new MeshPrimitive( vertsPerFace, vertIds, "linear", points );
+	}
+	return m;
+}
+
 Frame::Frame( GadgetPtr child )
 	:	IndividualContainer( child ), m_border( 1 )
 {
@@ -88,8 +148,29 @@ void Frame::doRender( IECore::RendererPtr renderer ) const
 	renderer->attributeBegin();
 	
 		renderer->setAttribute( "gl:curvesPrimitive:useGLLines", new IECore::BoolData( true ) );
+		renderer->setAttribute( "color", new IECore::Color3fData( Color3f( 0.2 ) ) );
 	
 		M44f m;
+		MeshPrimitivePtr rectangle = rectangleMesh();
+		V3fVectorDataPtr p = static_pointer_cast<V3fVectorData>( rectangle->variables["P"].data );
+		p->writable()[0] = V3f( b.min.x, b.min.y - m_border, 0 );
+		p->writable()[1] = V3f( b.max.x, b.min.y - m_border, 0 );
+		p->writable()[2] = V3f( b.max.x, b.max.y + m_border, 0 );
+		p->writable()[3] = V3f( b.min.x, b.max.y + m_border, 0 );
+		rectangle->render( renderer );
+		
+		p->writable()[0] = V3f( b.min.x - m_border, b.min.y, 0 );
+		p->writable()[1] = V3f( b.min.x, b.min.y, 0 );
+		p->writable()[2] = V3f( b.min.x, b.max.y, 0 );
+		p->writable()[3] = V3f( b.min.x - m_border, b.max.y, 0 );
+		rectangle->render( renderer );
+		
+		p->writable()[0] = V3f( b.max.x, b.min.y, 0 );
+		p->writable()[1] = V3f( b.max.x + m_border, b.min.y, 0 );
+		p->writable()[2] = V3f( b.max.x + m_border, b.max.y, 0 );
+		p->writable()[3] = V3f( b.max.x, b.max.y, 0 );
+		rectangle->render( renderer );
+		
 		renderer->transformBegin();
 			m.setTranslation( V3f( b.max.x, b.max.y, 0 ) );
 			renderer->concatTransform( m );
