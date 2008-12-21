2d2b54644c68861e29c9348f6e8fa3de452cc384
john <john@d3a5e123-01df-480b-a3e5-5969b2df5ebc>
2008-12-21 04:47:12 +0000

    Added imports for path filters.

--- python/Gaffer/__init__.py
+++ python/Gaffer/__init__.py
@@ -4,5 +4,6 @@ from AddNode import AddNode
 from WeakMethod import WeakMethod
 from Path import Path
 from FileSystemPath import FileSystemPath
+from PathFilter import PathFilter
 from BlockedConnection import BlockedConnection
-
+from FileNamePathFilter import FileNamePathFilter
