07d063b56be73e17f07217701d612aea6c526e67
john <john@d3a5e123-01df-480b-a3e5-5969b2df5ebc>
2008-12-29 07:46:22 +0000

    Added control over appearance of scroll bars.

--- python/GafferUI/ScrolledContainer.py
+++ python/GafferUI/ScrolledContainer.py
@@ -1,12 +1,19 @@
 import gtk
 
+import IECore
+
 import GafferUI
 
 class ScrolledContainer( GafferUI.ContainerWidget ) :
 
-	def __init__( self ) :
+	ScrollMode = IECore.Enum.create( "Never", "Always", "Automatic" )
+
+	def __init__( self, horizontalMode=ScrollMode.Automatic, verticalMode=ScrollMode.Automatic ) :
 	
 		GafferUI.ContainerWidget.__init__( self, gtk.ScrolledWindow() )
+		
+		self.setHorizontalMode( horizontalMode )
+		self.setVerticalMode( verticalMode )
 		
 		self.__child = None
 		
@@ -38,3 +45,38 @@ class ScrolledContainer( GafferUI.ContainerWidget ) :
 	def getChild( self ) :
 	
 		return self.__child
+	
+	__modesToPolicies = {
+		ScrollMode.Never : gtk.POLICY_NEVER,
+		ScrollMode.Always : gtk.POLICY_ALWAYS,
+		ScrollMode.Automatic : gtk.POLICY_AUTOMATIC,
+	}
+
+	__policiesToModes = {
+		gtk.POLICY_NEVER : ScrollMode.Never,
+		gtk.POLICY_ALWAYS : ScrollMode.Always,
+		gtk.POLICY_AUTOMATIC : ScrollMode.Automatic,
+	}
+		
+	def setHorizontalMode( self, mode ) :
+	
+		p = self.gtkWidget().get_policy()
+		self.gtkWidget().set_policy( self.__modesToPolicies[mode], p[1] )
+
+	def getHorizontalMode( self ) :
+	
+		p = self.gtkWidget().get_policy()
+		return self.__policiesToModes[p[0]]
+		
+	def setVerticalMode( self, mode ) :
+	
+		p = self.gtkWidget().get_policy()
+		self.gtkWidget().set_policy( p[0], self.__modesToPolicies[mode] )
+
+	def getVerticalMode( self ) :
+	
+		p = self.gtkWidget().get_policy()
+		return self.__policiesToModes[p[1]]
+		
+		
+		
