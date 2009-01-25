cc1aebfd2989ab5cd1e6a2cdd7feae1aff56ff8c
john <john@d3a5e123-01df-480b-a3e5-5969b2df5ebc>
2009-01-25 17:29:12 +0000

    Added the layout startup file.

---
+++ startup/ui/layouts.py
@@ -0,0 +1,3 @@
+import GafferUI
+
+GafferUI.Layouts.add( "Default", GafferUI.CompoundEditor( children = ( GafferUI.Splittable.SplitDirection.Vertical, ( ( GafferUI.Splittable.SplitDirection.None, (GafferUI.GraphEditor(),) ), ( GafferUI.Splittable.SplitDirection.Horizontal, ( ( GafferUI.Splittable.SplitDirection.None, (GafferUI.NodeEditor(),) ), ( GafferUI.Splittable.SplitDirection.None, (GafferUI.ScriptEditor(),) ) ) ) ) ) ) )
