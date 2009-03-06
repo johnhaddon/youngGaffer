a8589c43fb6f3cf2107b25f8692628674587e1e9
john <john@d3a5e123-01df-480b-a3e5-5969b2df5ebc>
2009-03-06 06:43:17 +0000

    Better handling of the case where there isn't a suitable PlugValueWidget.

--- python/GafferUI/PlugWidget.py
+++ python/GafferUI/PlugWidget.py
@@ -6,6 +6,7 @@ import CamelCase
 
 ## \todo Make tooltips work
 ## \todo Useful popup menu on label - connect, disconnect, expressions etc
+## Or does that belong on the PlugValueWidget?
 class PlugWidget( Widget ) :
 
 	def __init__( self, plugOrPlugValueWidget, label=None, description=None ) :
@@ -16,10 +17,12 @@ class PlugWidget( Widget ) :
 	
 		if isinstance( plugOrPlugValueWidget, PlugValueWidget ) :
 			self.__valueWidget = plugOrPlugValueWidget
+			plug = self.__valueWidget.getPlug()
 		else :
 			self.__valueWidget = PlugValueWidget.create( plugOrPlugValueWidget )
+			plug = plugOrPlugValueWidget
 
-		label = gtk.Label( label or CamelCase.toSpaced( self.__valueWidget.getPlug().getName() ) )
+		label = gtk.Label( label or CamelCase.toSpaced( plug.getName() ) )
 		label.set_width_chars( 15 )
 		label.set_alignment( 1, 0.5 )
 
@@ -28,7 +31,8 @@ class PlugWidget( Widget ) :
 		self._setDefaultColors( labelEventBox, True )
 		
 		self.__gtkHBox.pack_start( labelEventBox, False )			
-		self.__gtkHBox.pack_start( self.__valueWidget.gtkWidget() )
+		if self.__valueWidget :
+			self.__gtkHBox.pack_start( self.__valueWidget.gtkWidget() )
 
 		if description :
 			self.__tooltips = gtk.Tooltips()
