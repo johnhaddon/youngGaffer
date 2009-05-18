5dd85f0958b73dacec543afac22ce57260a3b0f2
john <john@d3a5e123-01df-480b-a3e5-5969b2df5ebc>
2009-05-18 19:38:13 +0000

    EditMenu now provides access to cut and paste etc.

--- python/GafferUI/EditMenu.py
+++ python/GafferUI/EditMenu.py
@@ -9,14 +9,14 @@ def appendDefinitions( menuDefinition, prefix="" ) :
 	menuDefinition.append( prefix + "/Undo", { "command" : undo } )
 	menuDefinition.append( prefix + "/Redo", { "command" : redo } )
 	menuDefinition.append( prefix + "/UndoDivider", { "divider" : True } )
+	
+	menuDefinition.append( prefix + "/Cut", { "command" : cut } )
+	menuDefinition.append( prefix + "/Copy", { "command" : copy } )
+	menuDefinition.append( prefix + "/Paste", { "command" : paste } )
 	menuDefinition.append( prefix + "/Delete", { "command" : delete } )
+	menuDefinition.append( prefix + "/CutCopyPasteDeleteDivider", { "divider" : True } )
 
-	## \todo add the following menu items
-	#			
-	#			(	"/Edit/Cut", {} 	),
-	#			(	"/Edit/Copy", {} 	),
-	#			(	"/Edit/Paste", {} 	),
-	#			(	"/Edit/Select All", {} 	),
+	menuDefinition.append( prefix + "/Select All", { "command" : selectAll } )
 
 ## A function suitable as the command for an Edit/Undo menu item. It must
 # be invoked from a menu that has a ScriptWindow in its ancestry.
@@ -33,6 +33,35 @@ def redo( menu ) :
 	scriptWindow = menu.ancestor( GafferUI.ScriptWindow )
 	script = scriptWindow.getScript()
 	script.redo()
+
+## A function suitable as the command for an Edit/Cut menu item. It must
+# be invoked from a menu that has a ScriptWindow in its ancestry.
+def cut( menu ) :
+
+	scriptWindow = menu.ancestor( GafferUI.ScriptWindow )
+	script = scriptWindow.getScript()
+
+	with Gaffer.UndoContext( script ) :
+		script.cut( script.selection() )
+
+## A function suitable as the command for an Edit/Copy menu item. It must
+# be invoked from a menu that has a ScriptWindow in its ancestry.
+def copy( menu ) :
+
+	scriptWindow = menu.ancestor( GafferUI.ScriptWindow )
+	script = scriptWindow.getScript()
+	script.copy( script.selection() )
+
+## A function suitable as the command for an Edit/Paste menu item. It must
+# be invoked from a menu that has a ScriptWindow in its ancestry.
+def paste( menu ) :
+
+	scriptWindow = menu.ancestor( GafferUI.ScriptWindow )
+	script = scriptWindow.getScript()
+	
+	with Gaffer.UndoContext( script ) :
+	
+		script.paste()
 	
 ## A function suitable as the command for an Edit/Delete menu item. It must
 # be invoked from a menu that has a ScriptWindow in its ancestry.
@@ -43,7 +72,15 @@ def delete( menu ) :
 	
 	with Gaffer.UndoContext( script ) :
 		
-		for node in script.selection().members() :
-		
-			script.deleteNode( node )
+		script.deleteNodes( script.selection() )
 	
+## A function suitable as the command for an Edit/Select All menu item. It must
+# be invoked from a menu that has a ScriptWindow in its ancestry.
+def selectAll( menu ) :
+
+	scriptWindow = menu.ancestor( GafferUI.ScriptWindow )
+	script = scriptWindow.getScript()
+	
+	for c in script.children() :
+		if c.isInstanceOf( Gaffer.Node.staticTypeId() ) :
+			script.selection().add( c )	
