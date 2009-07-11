6582e66f5fe4c0dbc58b9752baa367939b7f0e86
john <john@d3a5e123-01df-480b-a3e5-5969b2df5ebc>
2009-07-11 22:23:40 +0000

    Added a failing serialisation test.

--- python/GafferTest/SplinePlugTest.py
+++ python/GafferTest/SplinePlugTest.py
@@ -40,6 +40,34 @@ class SplinePlugTest( unittest.TestCase ) :
 		
 		self.assertEqual( p.getValue(), s2 ) 
 
+	def testSerialisation( self ) :
+	
+		s = IECore.Splineff(
+			IECore.CubicBasisf.catmullRom(),
+			(
+				( 0, 0 ),
+				( 0, 0 ),
+				( 0.2, 0.3 ),
+				( 0.4, 0.9 ),
+				( 1, 1 ),
+				( 1, 1 ),
+			)
+		)
+	
+		p = Gaffer.SplineffPlug( "a", defaultValue=s, flags=Gaffer.Plug.Flags.Dynamic )
+		self.assertEqual( p.getValue(), s )
+		
+		sn = Gaffer.ScriptNode()
+		sn["n"] = Gaffer.Node()
+		sn["n"]["p"] = p
+		
+		se = sn.serialise()
+		
+		sn = Gaffer.ScriptNode()
+		sn.execute( se )
+		
+		self.assertEqual( sn["n"]["p"].getValue(), s )
+
 if __name__ == "__main__":
 	unittest.main()
 	
