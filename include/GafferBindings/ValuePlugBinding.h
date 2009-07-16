605fd5b92d4c5a111f0b4be0c0488ccf61079eb6
john <john@d3a5e123-01df-480b-a3e5-5969b2df5ebc>
2009-07-16 06:59:32 +0000

    Added a note.

--- include/GafferBindings/ValuePlugBinding.h
+++ include/GafferBindings/ValuePlugBinding.h
@@ -9,6 +9,8 @@
 namespace GafferBindings
 {
 
+/// \todo I wonder if this should go in Serialiser anyway, and have a
+/// registration mechanism?
 std::string serialisePlugValue( Serialiser &s, Gaffer::PlugPtr plug );
 
 void setPlugValue( Gaffer::PlugPtr plug, boost::python::object value );
